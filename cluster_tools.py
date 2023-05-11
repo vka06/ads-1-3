aaf80bfdb9be5e216ffb9363fe982cf7c9418a98
vka06 <vasanthakavya06@gmail.com>
2023-05-11 10:29:54 +0100

    uploading

---
+++ cluster_tools.py
@@ -0,0 +1,83 @@
+""" Tools to support clustering: correlation heatmap, normaliser and scale 
+(cluster centres) back to original scale, check for mismatching entries """
+
+
+def map_corr(df, size=6):
+    """Function creates heatmap of correlation matrix for each pair of 
+    columns in the dataframe.
+
+    Input:
+        df: pandas DataFrame
+        size: vertical and horizontal size of the plot (in inch)
+        
+    The function does not have a plt.show() at the end so that the user 
+    can savethe figure.
+    """
+
+    import matplotlib.pyplot as plt  # ensure pyplot imported
+
+    corr = df.corr()
+    plt.figure(figsize=(size, size))
+    # fig, ax = plt.subplots()
+    plt.matshow(corr, cmap='coolwarm', location="bottom")
+    # setting ticks to column names
+    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+
+    plt.colorbar()
+    # no plt.show() at the end
+    
+    
+def scaler(df):
+    """ Expects a dataframe and normalises all 
+        columnsto the 0-1 range. It also returns 
+        dataframes with minimum and maximum for
+        transforming the cluster centres"""
+
+    # Uses the pandas methods
+    df_min = df.min()
+    df_max = df.max()
+
+    df = (df-df_min) / (df_max - df_min)
+
+    return df, df_min, df_max
+
+
+def backscale(arr, df_min, df_max):
+    """ Expects an array of normalised cluster centres and scales
+        it back. Returns numpy array.  """
+
+    # convert to dataframe to enable pandas operations
+    minima = df_min.to_numpy()
+    maxima = df_max.to_numpy()
+
+    # loop over the "columns" of the numpy array
+    for i in range(len(minima)):
+        arr[:, i] = arr[:, i] * (maxima[i] - minima[i]) + minima[i]
+
+    return arr
+
+
+def get_diff_entries(df1, df2, column):
+    """ Compares the values of column in df1 and the column with the same 
+    name in df2. A list of mismatching entries is returned. The list will be
+    empty if all entries match. """
+
+    import pandas as pd  # to be sure
+
+    # merge dataframes keeping all rows
+    df_out = pd.merge(df1, df2, on=column, how="outer")
+    print("total entries", len(df_out))
+    # merge keeping only rows in common
+    df_in = pd.merge(df1, df2, on=column, how="inner")
+    print("entries in common", len(df_in))
+    df_in["exists"] = "Y"
+
+    # merge again
+    df_merge = pd.merge(df_out, df_in, on=column, how="outer")
+
+    # extract columns without "Y" in exists
+    df_diff = df_merge[(df_merge["exists"] != "Y")]
+    diff_list = df_diff[column].to_list()
+
+    return diff_list
